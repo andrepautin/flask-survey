ebc8c7471a7937141847f69b73fd18d741cb1c5d
S T <itschibits@gmail.com>
2021-04-22 15:23:55 -0400

    survey start and first question

--- app.py
+++ app.py
@@ -7,3 +7,19 @@ app.config['SECRET_KEY'] = "never-tell!"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
+
+responses = []
+
+
+@app.route('/')
+def show_survey_instructions():
+    return render_template("survey_start.html", survey=survey)
+
+
+@app.route('/begin', methods=["POST"])
+def show_survey_questions():
+    return redirect("/questions/0")
+
+@app.route('/questions/0')
+def show_first_question():
+    return render_template("question.html", question=survey.questions[0])
